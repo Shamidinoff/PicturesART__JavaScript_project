66a732ee5490303e4a2c103c26f519544fe5a03e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-03 20:00:30 +0600

    PicturesART - JavaScript project

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,131 @@
 /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/modals.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/modals.js ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    let closeClickOverlay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll("[data-modal]"),
+      scroll = calcScroll();
+    trigger.forEach(item => {
+      item.addEventListener("click", e => {
+        if (e.target) {
+          e.preventDefault();
+        }
+        windows.forEach(item => {
+          item.style.display = "none";
+        });
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
+      });
+    });
+    close.addEventListener("click", () => {
+      windows.forEach(item => {
+        item.style.display = "none";
+      });
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
+    });
+    modal.addEventListener("click", e => {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+          item.style.display = "none";
+        });
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
+      }
+    });
+  }
+  function showModalByTime(selector, time) {
+    setTimeout(function () {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "hidden";
+    }, time);
+  }
+  function calcScroll() {
+    let div = document.createElement("div");
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflow = "scroll";
+    div.style.visibility = "hidden";
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
+  bindModal(".button-design", ".popup-design", ".popup-design .popup-close");
+};
+/* harmony default export */ __webpack_exports__["default"] = (modals);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_modals__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modals */ "./src/js/modules/modals.js");
 
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  (0,_modules_modals__WEBPACK_IMPORTED_MODULE_0__["default"])();
+});
+}();
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -24,7 +24,6 @@ const modals = () => {
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
         document.body.style.marginRight = `${scroll}px`;
-        //   document.body.classList.add("modal-open");
       });
     });
 
@@ -36,7 +35,6 @@ const modals = () => {
       modal.style.display = "none";
       document.body.style.overflow = "";
       document.body.style.marginRight = `0px`;
-      //   document.body.classList.remove("modal-open");
     });
 
     modal.addEventListener("click", (e) => {
@@ -48,7 +46,6 @@ const modals = () => {
         modal.style.display = "none";
         document.body.style.overflow = "";
         document.body.style.marginRight = `0px`;
-        // document.body.classList.remove("modal-open");
       }
     });
   }
@@ -74,28 +71,7 @@ const modals = () => {
     return scrollWidth;
   }
 
-  bindModal(
-    ".popup_engineer_btn",
-    ".popup_engineer",
-    ".popup_engineer .popup_close"
-  );
-
-  bindModal(".phone_link", ".popup", ".popup .popup_close");
-  bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close", false);
-  bindModal(
-    ".popup_calc_button",
-    ".popup_calc_profile",
-    ".popup_calc_profile_close",
-    false
-  );
-  bindModal(
-    ".popup_calc_profile_button",
-    ".popup_calc_end",
-    ".popup_calc_end_close",
-    false
-  );
-
-  //   showModalByTime(".popup", 60000);
+  bindModal(".button-design", ".popup-design", ".popup-design .popup-close");
 };
 
 export default modals;
