331dcef43eea09916f947dae6f9b3825a2d6eeaa
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-07 19:27:11 +0600

    PicturesART - JavaScript project

---
+++ src/js/modules/calc.js
@@ -0,0 +1,9 @@
+const calc = (size, material, options, promocode, result) => {
+  const sizeBlock = document.querySelector(size),
+    materialBlock = document.querySelector(material),
+    optionsBlock = document.querySelector(options),
+    promocodeBlock = document.querySelector(promocode),
+    resultBlock = document.querySelector(result);
+};
+
+export default calc;
