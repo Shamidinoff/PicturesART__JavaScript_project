1726922f47fd83ead467869b46e31d600a1b3b1e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-07 19:37:01 +0600

    PicturesART - JavaScript project

--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -4,6 +4,28 @@ const calc = (size, material, options, promocode, result) => {
     optionsBlock = document.querySelector(options),
     promocodeBlock = document.querySelector(promocode),
     resultBlock = document.querySelector(result);
+
+  let sum = 0;
+
+  const calcFunc = () => {
+    sum = Math.round(
+      +sizeBlock.value * +materialBlock.value + +optionsBlock.value
+    );
+
+    if (sizeBlock.value == "" || materialBlock.value == "") {
+      resultBlock.textContent =
+        "Пожалуйста, выберите размер и материал картины";
+    } else if (promocodeBlock.value === "IWANTPOPART") {
+      resultBlock.textContent = Math.round(sum * 0.7);
+    } else {
+      resultBlock.textContent = sum;
+    }
+  };
+
+  sizeBlock.addEventListener("change", calcFunc);
+  materialBlock.addEventListener("change", calcFunc);
+  optionsBlock.addEventListener("change", calcFunc);
+  promocodeBlock.addEventListener("change", calcFunc);
 };
 
 export default calc;
