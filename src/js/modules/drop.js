34e517592ea27acba9499872576f68a871b45661
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-08 19:58:27 +0600

    PicturesART - JavaScript project

--- src/js/modules/drop.js
+++ src/js/modules/drop.js
@@ -1,3 +1,26 @@
-const drop = () => {};
+const drop = () => {
+  const fileInputs = document.querySelectorAll('[name="upload"]');
+
+  ["dragenter", "dragleave", "dragover", "drag"].forEach((eventName) => {
+    fileInputs.forEach((input) => {
+      input.addEventListener(eventName, preventDefaults, false);
+    });
+  });
+
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function highlight(item) {
+    item.closest(".file_upload").style.border = "5px solid yellow";
+    item.closest(".file_upload").style.backgroundColor = "rgba(0,0,0, .7)";
+  }
+
+  function unhighlight(item) {
+    item.closest(".file_upload").style.border = "none";
+    item.closest(".file_upload").style.backgroundColor = "#ededed";
+  }
+};
 
 export default drop;
