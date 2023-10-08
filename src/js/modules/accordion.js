1a8818dde136b4b47e3bc6b2fedc4882ee0a64c5
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-08 16:56:08 +0600

    PicturesART - JavaScript project

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,3 +1,21 @@
-const accordion = () => {};
+const accordion = (triggersSelector, itemsSelector) => {
+  const btns = document.querySelectorAll(triggersSelector),
+    blocks = document.querySelectorAll(itemsSelector);
+
+  blocks.forEach((block) => {
+    block.classList.add("animated", "fadeInDown");
+  });
+
+  btns.addEventListener((btn) => {
+    btn.addEventListener("clock", function () {
+      if (!this.classList.contains("active")) {
+        btns.forEach((btn) => {
+          btn.classList.remove("active", "active-style");
+        });
+        this.classList.add("active", "active-style");
+      }
+    });
+  });
+};
 
 export default accordion;
