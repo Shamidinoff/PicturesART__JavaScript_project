8d8e7e1c3bfc8d9d4e8472e9c2a7b15f148f7259
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-08 18:53:35 +0600

    PicturesART - JavaScript project

--- dist/script.js
+++ dist/script.js
@@ -488,7 +488,7 @@ const pictureSize = imgSelector => {
 __webpack_require__.r(__webpack_exports__);
 const scrolling = upSelector => {
   const upElem = document.querySelector(upSelector);
-  window.addEventListener("scroll", upSelector => {
+  window.addEventListener("scroll", () => {
     if (document.documentElement.scrollTop > 1650) {
       upElem.classList.add("animated", "fadeIn");
       upElem.classList.remove("fadeOut");
@@ -497,7 +497,92 @@ const scrolling = upSelector => {
       upElem.classList.remove("fadeIn");
     }
   });
+
+  // Scrolling with raf
+
+  let links = document.querySelectorAll('[href^="#"]'),
+    speed = 0.3;
+  links.forEach(link => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      let widthTop = document.documentElement.scrollTop,
+        hash = this.hash,
+        toBlock = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+      requestAnimationFrame(step);
+      function step(time) {
+        if (start === null) {
+          start = time;
+        }
+        let progress = time - start,
+          r = toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock);
+        document.documentElement.scrollTo(0, r);
+        if (r != widthTop + toBlock) {
+          requestAnimationFrame(step);
+        } else {
+          location.hash = hash;
+        }
+      }
+    });
+  });
+
+  // Pure js scrolling
+
+  // const element = document.documentElement,
+  //       body = document.body;
+
+  // const calcScroll = () => {
+  //     upElem.addEventListener('click', function(event) {
+  //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+  //         if (this.hash !== '') {
+  //             event.preventDefault();
+  //             let hashElement = document.querySelector(this.hash),
+  //                 hashElementTop = 0;
+
+  //             while (hashElement.offsetParent) {
+  //                 hashElementTop += hashElement.offsetTop;
+  //                 hashElement = hashElement.offsetParent;
+  //             }
+
+  //             hashElementTop = Math.round(hashElementTop);
+  //             smoothScroll(scrollTop, hashElementTop, this.hash);
+  //         }
+  //     });
+  // };
+
+  // const smoothScroll = (from, to, hash) => {
+  //     let timeInterval = 1,
+  //         prevScrollTop,
+  //         speed;
+
+  //     if (to > from) {
+  //         speed = 30;
+  //     } else {
+  //         speed = -30;
+  //     }
+
+  //     let move = setInterval(function() {
+  //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+  //         if (
+  //             prevScrollTop === scrollTop ||
+  //             (to > from && scrollTop >= to) ||
+  //             (to < from && scrollTop <= to)
+  //         ) {
+  //             clearInterval(move);
+  //             history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+  //         } else {
+  //             body.scrollTop += speed;
+  //             element.scrollTop += speed;
+  //             prevScrollTop = scrollTop;
+  //         }
+  //     }, timeInterval);
+  // };
+
+  // calcScroll();
 };
+
 /* harmony default export */ __webpack_exports__["default"] = (scrolling);
 
 /***/ }),
